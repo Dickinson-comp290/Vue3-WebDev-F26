be73a7a94fb219eb264a99555b77e82a228f6594
mandygrawl <grawla@dickinson.edu>
2026-09-08 17:34:52 +0000

    final commit

--- first-website/scripts/main.js
+++ first-website/scripts/main.js
@@ -15,14 +15,14 @@ let myHeading = document.querySelector('h3');
 function setUserName() {
   const myName = prompt('Please enter your name.');
   localStorage.setItem('name', myName);
-  myHeading.textContent = `Welcome to the Space Station, ${myName}`;
+  myHeading.textContent = `Welcome to the Space Station, ${myName}!`;
 }
 
 if (!localStorage.getItem('name')) {
   setUserName();
 } else {
   const storedName = localStorage.getItem('name');
-  myHeading.textContent = `Welcome back to the Space Station, ${storedName}`;
+  myHeading.textContent = `Welcome back to the Space Station, ${storedName}.`;
 }
 
 myButton.addEventListener('click', () => {
