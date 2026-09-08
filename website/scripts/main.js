8885b72a692d96542db74c83f42bb453fb41ede0
Jules Borusiewicz <jborus27@gmail.com>
2026-09-08 21:51:07 +0000

    js finished

--- website/scripts/main.js
+++ website/scripts/main.js
@@ -8,3 +8,33 @@ myImage.addEventListener('click', () => {
     myImage.setAttribute('src', 'images/explode.png');
   }
 });
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  const myName = prompt('Please enter your name.');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = `welcome to dice roller, ${myName}`;
+}
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name');
+  myHeading.textContent = `welcome to dice roller, ${storedName}`;
+}
+
+myButton.addEventListener('click', () => {
+  setUserName();
+});
+
+function setUserName() {
+  const myName = prompt('Please enter your name.');
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `welcome to dice roller, ${myName}`;
+  }
+}
