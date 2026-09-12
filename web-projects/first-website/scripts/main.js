e4d81707f9a88fff2a24d801c9653f23630df44a
anna mags <magarela@dickinson.edu>
2026-09-12 23:32:40 +0000

    new nickname with the Ex01

--- web-projects/first-website/scripts/main.js
+++ web-projects/first-website/scripts/main.js
@@ -32,3 +32,7 @@ if (!localStorage.getItem('name')) {
 myButton.addEventListener('click', () => {
   setUserName();
 });
+const dateControl = document.querySelector('input[type="date"]');
+dateControl.value = '2017-06-01';
+console.log(dateControl.value); // prints "2017-06-01"
+console.log(dateControl.valueAsNumber); // prints 1496275200000, a JavaScript timestamp (ms)
