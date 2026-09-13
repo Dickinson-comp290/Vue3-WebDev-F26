2ee7060e848e21a0825978631a5c8e4af71b88a5
JustJoe05 <mullenj@dickinson.edu>
2026-09-13 21:51:21 +0000

    fixed images

--- web-projects/first-website/scripts/main.js
+++ web-projects/first-website/scripts/main.js
@@ -26,7 +26,7 @@ if (!localStorage.getItem('name')) {
   setUserName();
 } else {
   const storedName = localStorage.getItem('name');
-  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  myHeading.textContent = `Look at my cool website, ${storedName}`;
 }
 
 myButton.addEventListener('click', () => {
