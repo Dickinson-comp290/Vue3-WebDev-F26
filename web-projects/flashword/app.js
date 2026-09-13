ca56adbd7a6ba6e3619fb3044910363626b59ca2
linhtruong309 <truongh@dickinson.edu>
2026-09-13 23:32:40 +0000

    completed "the Basics"

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -1,4 +1,3 @@
-// Define the options of our application
 const FlashWord = {
   data() {
     return {
@@ -9,9 +8,14 @@ const FlashWord = {
       showFeedback: false,
     };
   },
+  methods: {
+    checkAnswer() {
+      // Note how data properties are accessed via `this`
+      this.correct = this.answer == this.wordB;
+      this.showFeedback = true;
+    },
+  },
 };
 
 // Create a new Vue instance using our options
 const app = Vue.createApp(FlashWord).mount('#app');
-
-window.app = app;
