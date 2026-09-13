7d8806cf0a335b9b63d8ed090513b7b08931ed3c
brigidfoley <foleyb@dickinson.edu>
2026-09-13 21:05:15 +0000

    t01 done

--- web-projects/first-website/javascript.js
+++ web-projects/first-website/javascript.js
@@ -8,3 +8,42 @@ myImage.addEventListener('click', () => {
     myImage.setAttribute('src', 'samoyed.png');
   }
 });
+
+const listItems = document.querySelectorAll('li');
+
+function toggleDone(e) {
+  if (!e.target.className) {
+    e.target.className = 'done';
+  } else {
+    e.target.className = '';
+  }
+}
+
+listItems.forEach((item) => {
+  item.addEventListener('click', toggleDone);
+});
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myButton.addEventListener('click', () => {
+  setUserName();
+});
+
+if (!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem('name');
+  myHeading.textContent = `Welcome, ${storedName}`;
+}
+
+function setUserName() {
+  const myName = prompt('Please enter your name.');
+  if (!myName) {
+    setUserName();
+    localStorage.setItem('name', myName);
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = `Welcome, ${myName}`;
+  }
+}
