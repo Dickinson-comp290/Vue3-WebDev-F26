7f546f4c50e19cda17ae91f9322d6df2882b0bc9
anna mags <magarela@dickinson.edu>
2026-09-15 23:01:47 +0000

    adding a list

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -6,6 +6,19 @@ const FlashWord = {
       wordB: 'hello',
       answer: '',
       showFeedback: false,
+      // Array example
+      spanishWords: ['hola', 'adios', 'uno', 'dos'],
+
+      // Object example
+      word: { a: 'hola', b: 'hello' },
+
+      // Array of objects example
+      words: [
+        { wordA: 'hola', wordB: 'hello' },
+        { wordA: 'adios', wordB: 'goodbye' },
+        { wordA: 'uno', wordB: 'one' },
+        { wordA: 'dos', wordB: 'two' },
+      ],
     };
   },
   methods: {
