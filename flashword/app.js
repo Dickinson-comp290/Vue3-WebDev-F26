9ae6538aa886747bf029756226a47cd28505c9cd
anna mags <magarela@dickinson.edu>
2026-09-15 13:05:16 +0000

    commenting out temporarily

---
+++ flashword/app.js
@@ -0,0 +1,9 @@
+// const FlashWord = {
+//     data(){
+//         return{
+//             wordA: 'hola',
+//             wordB: 'hello'
+//         }
+//     }
+// }
+// //const app = Vue.createApp(FlashWord).mount('#app');
