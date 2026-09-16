1d8e52a33973c2a00047d2f0b387bd77e4a3a6bf
halpinp2000 <halpinp@dickinson.edu>
2026-09-16 05:06:19 +0000

    fixed linting

---
+++ web-projects/flashword/app.js
@@ -0,0 +1,25 @@
+const FlashWord = {
+  data() {
+    return {
+      wordA: 'hola',
+      wordB: 'hello',
+      answer: '',
+      correct: null,
+      showFeedback: false,
+    };
+  },
+  methods: {
+    checkAnswer() {
+      this.correct = this.wordB == this.answer;
+      this.showFeedback = true;
+    },
+
+    resetAnswer() {
+      this.answer = '';
+      this.showFeedback = false;
+    },
+  },
+};
+
+// eslint-disable-next-line no-unused-vars, no-undef
+const app = Vue.createApp(FlashWord).mount('#app');
