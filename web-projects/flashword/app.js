49b65dee3536b300cd2ba6bd857fe3a91b647804
halpinp2000 <halpinp@dickinson.edu>
2026-09-16 07:27:07 +0000

    Finished List Rendering

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -6,8 +6,21 @@ const FlashWord = {
       answer: '',
       correct: null,
       showFeedback: false,
+      spanishWords: ['hola', 'adios', 'uno', 'dos'],
+
+      // Object example
+      word: { a: 'hola', b: 'hello' },
+
+      // Array of objects example
+      words: [
+        { wordA: 'hola', wordB: 'hello' },
+        { wordA: 'adios', wordB: 'goodbye' },
+        { wordA: 'uno', wordB: 'one' },
+        { wordA: 'dos', wordB: 'two' },
+      ],
     };
   },
+
   methods: {
     checkAnswer() {
       this.correct = this.wordB == this.answer;
