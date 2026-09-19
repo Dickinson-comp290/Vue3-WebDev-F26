6d76c6d2026d3ee9cd8060ad776e38acc7f7190d
linhtruong309 <truongh@dickinson.edu>
2026-09-19 02:26:29 +0000

    completed "event argument", committed this bc I have to delete it for next exercise.

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -20,19 +20,20 @@ const Flashword = {
         { wordA: 'uno', wordB: 'one' },
         { wordA: 'dos', wordB: 'two' },
       ],
-    }
+    };
   },
   methods: {
-    checkAnswer() {
-      this.correct = this.wordB == this.answer
-      this.showFeedback = true
+    checkAnswer(event) {
+      console.log(event);
+      this.correct = this.wordB == this.answer;
+      this.showFeedback = true;
     },
     reset() {
-      this.answer = ''
-      this.showFeedback = false
+      this.answer = '';
+      this.showFeedback = false;
     },
   },
-}
+};
 
 // eslint-disable-next-line no-unused-vars, no-undef
-const app = Vue.createApp(Flashword).mount('#app')
+const app = Vue.createApp(Flashword).mount('#app');
