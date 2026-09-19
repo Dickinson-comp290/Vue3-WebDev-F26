73f32dcea2a5563c25f77c4656b308f3fdb24f04
JustJoe05 <mullenj@dickinson.edu>
2026-09-19 20:16:38 +0000

    Tutorial 01 - HTML/CSS

---
+++ web-projects/flashword/app.js
@@ -0,0 +1,22 @@
+// Define the options of our application
+const FlashWord = {
+  data() {
+    return {
+      wordA: 'hola',
+      wordB: 'hello',
+      answer: '',
+      correct: null,
+      showFeedback: false,
+    };
+  },
+  methods: {
+    checkAnswer() {
+      this.correct = this.wordB == this.answer;
+      this.showFeedback = true;
+    },
+  },
+};
+
+// Create a new Vue instance using our options
+// eslint-disable-next-line no-unused-vars, no-undef
+const app = Vue.createApp(FlashWord).mount('#app');
