769c9bab3d6c4261ae8a60a67bced54023bfb771
Jules Borusiewicz <jborus27@gmail.com>
2026-09-20 23:08:54 +0000

    added all completion items

--- website-projects/flashword/app.js
+++ website-projects/flashword/app.js
@@ -7,22 +7,29 @@ const FlashWord = {
       correct: null,
       showFeedback: false,
 
-        // Array example
-        spanishWords: ['hola', 'adios', 'uno', 'dos'],
+      // Array example
+      spanishWords: ['hola', 'adios', 'uno', 'dos'],
 
-        // Object example
-        word: { a: 'hola', b: 'hello' },
+      // Object example
+      word: { a: 'hola', b: 'hello' },
 
-        // Array of objects example
-        words: [
-            { wordA: 'hola', wordB: 'hello' },
-            { wordA: 'adios', wordB: 'goodbye' },
-            { wordA: 'uno', wordB: 'one' },
-            { wordA: 'dos', wordB: 'two' },
-        ],
+      // Array of objects example
+      words: [
+        { wordA: 'hola', wordB: 'hello' },
+        { wordA: 'adios', wordB: 'goodbye' },
+        { wordA: 'uno', wordB: 'one' },
+        { wordA: 'dos', wordB: 'two' },
+      ],
     };
   },
   methods: {
+    getRandomWord() {
+      const randomIndex = Math.floor(Math.random() * this.words.length);
+      const randomWord = this.words[randomIndex];
+      this.wordA = randomWord.wordA;
+      this.wordB = randomWord.wordB;
+      this.reset();
+    },
     checkAnswer() {
       // Note how data properties are accessed via `this`
       this.correct = this.answer == this.wordB;
@@ -30,9 +37,11 @@ const FlashWord = {
     },
     reset() {
       this.answer = '';
+      this.correct = null;
       this.showFeedback = false;
     },
   },
 };
-// Create a new Vue instance using our options
+
+// eslint-disable-next-line no-unused-vars, no-undef
 const app = Vue.createApp(FlashWord).mount('#app');
