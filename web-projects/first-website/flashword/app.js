fef66a74df46a44dea4794fa2c3b8f5dd397f288
brigidfoley <foleyb@dickinson.edu>
2026-09-23 02:45:58 +0000

    fixed the reset button so now it works

---
+++ web-projects/first-website/flashword/app.js
@@ -0,0 +1,27 @@
+const FlashWord = {
+  data() {
+    return {
+      wordA: 'hola',
+      wordB: 'hello',
+      answer: '',
+      correct: null,
+      showFeedback: false,
+    };
+  },
+  methods: {
+    checkAnswer() {
+      // Note how data properties are accessed via `this`
+      this.correct = this.answer == this.wordB;
+      this.showFeedback = true;
+    },
+
+    reset() {
+      this.answer = '';
+      this.showFeedback = false;
+    },
+  },
+};
+
+const app = Vue.createApp(FlashWord).mount('#app');
+
+// Create a new Vue instance using our options
