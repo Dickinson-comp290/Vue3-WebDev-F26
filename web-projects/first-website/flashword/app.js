1b493cb9d84376989a29a40c5104854d86b69667
brigidfoley <foleyb@dickinson.edu>
2026-09-23 03:51:06 +0000

    The game now has many words you can cycle through

--- web-projects/first-website/flashword/app.js
+++ web-projects/first-website/flashword/app.js
@@ -1,14 +1,39 @@
 const FlashWord = {
   data() {
     return {
-      wordA: 'hola',
-      wordB: 'hello',
+      words: [
+        { span: 'hola', eng: 'hello' },
+        { span: 'adiós', eng: 'goodbye' },
+        { span: 'gracias', eng: 'thank you' },
+        { span: 'por favor', eng: 'please' },
+        { span: 'perro', eng: 'dog' },
+        { span: 'gato', eng: 'cat' },
+        { span: 'casa', eng: 'house' },
+        { span: 'escuela', eng: 'school' },
+        { span: 'libro', eng: 'book' },
+        { span: 'comida', eng: 'food' },
+        { span: 'agua', eng: 'water' },
+        { span: 'amigo', eng: 'friend' },
+        { span: 'familia', eng: 'family' },
+      ],
+      wordA: '',
+      wordB: '',
       answer: '',
       correct: null,
       showFeedback: false,
     };
   },
+
   methods: {
+    pickWord() {
+      const randomIndex = Math.floor(Math.random() * this.words.length);
+      this.wordA = this.words[randomIndex].span;
+      this.wordB = this.words[randomIndex].eng;
+      this.answer = '';
+      this.showFeedback = false;
+      this.correct = null;
+    },
+
     checkAnswer() {
       // Note how data properties are accessed via `this`
       this.correct = this.answer == this.wordB;
@@ -19,10 +44,9 @@ const FlashWord = {
       this.answer = '';
       this.showFeedback = false;
       this.correct = null;
+      this.pickWord();
     },
   },
 };
 
 const app = Vue.createApp(FlashWord).mount('#app');
-
-// Create a new Vue instance using our options
