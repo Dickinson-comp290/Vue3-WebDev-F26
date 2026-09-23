a8a832dc2f3cf083bc60c6867cd20e9eb5d6b523
halpinp2000 <halpinp@dickinson.edu>
2026-09-23 04:57:17 +0000

    Added more interactive elements
    Used Copilot as required:

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -1,8 +1,8 @@
 const Flashword = {
   data() {
     return {
-      wordA: 'hola',
-      wordB: 'hello',
+      wordA: '',
+      wordB: '',
       answer: '',
       correct: null,
       showFeedback: false,
@@ -20,19 +20,31 @@ const Flashword = {
         { wordA: 'uno', wordB: 'one' },
         { wordA: 'dos', wordB: 'two' },
       ],
-    }
+    };
+  },
+  created() {
+    this.words = this.words.sort(() => Math.random() - 0.5);
+    this.wordA = this.words[0].wordA;
+    this.wordB = this.words[0].wordB;
   },
   methods: {
     checkAnswer() {
-      this.correct = this.wordB == this.answer
-      this.showFeedback = true
+      this.correct = this.wordB == this.answer;
+      this.showFeedback = true;
     },
     reset() {
-      this.answer = ''
-      this.showFeedback = false
+      this.answer = '';
+      this.showFeedback = false;
+      this.correct = null;
+    },
+    newWord() {
+      this.reset();
+      this.words = this.words.sort(() => Math.random() - 0.5);
+      this.wordA = this.words[0].wordA;
+      this.wordB = this.words[0].wordB;
     },
   },
-}
+};
 
 // eslint-disable-next-line no-unused-vars, no-undef
-const app = Vue.createApp(Flashword).mount('#app')
+const app = Vue.createApp(Flashword).mount('#app');
