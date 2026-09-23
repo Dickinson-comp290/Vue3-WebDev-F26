8716e8739a2a4a5e0505e1082177fe591933edff
brigidfoley <foleyb@dickinson.edu>
2026-09-23 04:18:52 +0000

    Event handler added 'v-on:keyup.enter'

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -20,19 +20,19 @@ const Flashword = {
         { wordA: 'uno', wordB: 'one' },
         { wordA: 'dos', wordB: 'two' },
       ],
-    }
+    };
   },
   methods: {
     checkAnswer() {
-      this.correct = this.wordB == this.answer
-      this.showFeedback = true
+      this.correct = this.wordB == this.answer;
+      this.showFeedback = true;
     },
     reset() {
-      this.answer = ''
-      this.showFeedback = false
+      this.answer = '';
+      this.showFeedback = false;
     },
   },
-}
+};
 
 // eslint-disable-next-line no-unused-vars, no-undef
-const app = Vue.createApp(Flashword).mount('#app')
+const app = Vue.createApp(Flashword).mount('#app');
