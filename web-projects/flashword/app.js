7cef60a19afadb2fe56cd371dc835cf72deac5c6
linhtruong309 <truongh@dickinson.edu>
2026-09-25 21:13:38 +0000

    sets clean starting point for e03

--- web-projects/flashword/app.js
+++ web-projects/flashword/app.js
@@ -6,74 +6,46 @@ const Flashword = {
       answer: '',
       correct: null,
       showFeedback: false,
-      image: null,
-      imageAlt: null,
       hasError: false,
       inputBackgroundColor: 'white',
-      showHint: false,
-      categories: ['greetings', 'colors', 'verbs'],
-      level: 'easy',
-      sentence: 'abc',
-      firstName: '',
-      lastName: '',
-      firstNameAndLastName: '',
-      spanishWords: ['hola', 'adios', 'uno', 'dos'],
-
-      // Object example
-      word: { a: 'hola', b: 'hello' },
 
       // Array of objects example
-      // words: [
-      //   { wordA: 'hola', wordB: 'hello' },
-      //   { wordA: 'adios', wordB: 'goodbye' },
-      //   { wordA: 'uno', wordB: 'one' },
-      //   { wordA: 'dos', wordB: 'two' },
-      // ],
+      words: [
+        { wordA: 'hola', wordB: 'hello' },
+        { wordA: 'adios', wordB: 'goodbye' },
+        { wordA: 'uno', wordB: 'one' },
+        { wordA: 'dos', wordB: 'two' },
+      ],
     };
   },
-  computed: {
-    fullName() {
-      return this.firstName + ' ' + this.lastName;
-    },
-    shortSpanishWords() {
-      return this.spanishWords.filter((word) => word.length <= 3);
-    },
-  },
-  watch: {
-    firstName() {
-      this.firstNameAndLastName = this.firstName + ' ' + this.lastName;
-    },
-    lastName() {
-      this.firstNameAndLastName = this.firstName + ' ' + this.lastName;
-    },
-  },
+  watch: {},
+  computed: {},
   methods: {
-    getFullName() {
-      return this.firstName + ' ' + this.lastName;
-    },
     checkAnswer() {
       if (this.answer == '') {
         this.hasError = true;
-        this.inputBackgroundColor = 'pink';
+        this.inputBackgroundColor = 'lightpink';
         return;
       }
+
       this.hasError = false;
       this.inputBackgroundColor = 'white';
+
       this.correct = this.wordB == this.answer;
-      if (this.correct) {
-        this.circleType = 'correct';
-        this.imageAlt = 'Green check mark';
-      } else {
-        this.circleType = 'incorrect';
-        this.imageAlt = 'Red x mark';
-      }
+
       this.showFeedback = true;
     },
     reset() {
       this.answer = '';
       this.showFeedback = false;
-      this.image = null;
-      this.imageAlt = null;
+      this.correct = null;
+      this.inputBackgroundColor = 'white';
+      this.hasError = false;
+
+      // Reset to a new random word
+      const randomIndex = Math.floor(Math.random() * this.words.length);
+      this.wordA = this.words[randomIndex].wordA;
+      this.wordB = this.words[randomIndex].wordB;
     },
   },
 };
